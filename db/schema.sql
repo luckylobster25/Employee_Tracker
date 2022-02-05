68c6b61e923d4f2229a9d58bca1c2227ec33f812
Chue Chang <chuechang1996@outlook.com>
2022-02-05 01:43:47 -0600

    fix seed and schema file

--- db/schema.sql
+++ db/schema.sql
@@ -22,11 +22,12 @@ CREATE TABLE employee (
   id INT PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  role_id INT,
+  role_id INT NOT NULL,
   manager_id INT,
   FOREIGN KEY (role_id)
-  REFERENCES role(id)
-  ON DELETE SET NULL
+  REFERENCES role(id),
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id) 
 );
 
 SHOW TABLES;
